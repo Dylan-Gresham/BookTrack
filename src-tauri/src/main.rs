6a6746aa943073dc4871e679cf9827e9b2c2dc64
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-10-14 21:58:40 -0600

    Move db into `DB` struct

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,27 +3,32 @@
 
 use libsql_client::Client;
 
+struct DB {
+    connection: Client
+}
+
+impl DB {
+    async fn create_db_connection(db_url: &str, db_auth_token: &str) -> Self {
+        Self {
+            connection: libsql_client::Client::from_config(libsql_client::Config {
+                url: url::Url::parse(db_url).unwrap(),
+                auth_token: Some(String::from(db_auth_token)),
+            }).await.unwrap(),
+        }
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-async fn create_db_connection() -> Client {
-    let db = libsql_client::Client::from_config(libsql_client::Config {
-        url: url::Url::parse("libsql://booktrack-db-dylan-gresham.turso.io").unwrap(),
-        auth_token: Some(String::from("DB_TOKEN_HERE")),
-    }).await.unwrap();
-
-    return db;
-}
-
 fn main() {
-    let _db = create_db_connection();
-    // I think I need to create a DB struct that has all the database methods there
-    // that way the entire struct has access to the connection and that way I don't
-    // need to figure out how to make a global connection variable
-
+    let _db = DB::create_db_connection(
+        "libsql://booktrack-db-dylan-gresham.turso.io",
+         "TOKEN");
+    
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
