fac534334279eb55e8efe1709747f4afb5b3f1cb
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-12-11 22:27:54 -0700

    Implement dir creation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use libsql_client::Client;
+use std::fs;
 
 struct DB {
     _connection: Client
@@ -24,14 +25,33 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn update_db_connection(_db_token: &str) -> () {
+    todo!()
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), String> {
     let _db = DB::create_db_connection(
         "libsql://booktrack-db-dylan-gresham.turso.io",
-         "TOKEN");
-    
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+         "TOKEN").await;
+
+    let dir_creation_result = fs::create_dir_all("~/config/BookTrack/");
+    let (dir_creation_continue, error) = match dir_creation_result {
+        Ok(_) => (true, String::from("")),
+        Err(e) => {
+            eprintln!("Error encountered while attempting to create ~/config/BookTrack/ : {e}");
+            (false, e.to_string())
+        }
+    };
+
+    if dir_creation_continue {
+        tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, update_db_connection])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+        Ok(())
+    } else {
+        Err(error)
+    }
 }
