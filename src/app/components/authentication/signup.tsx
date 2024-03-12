7e1b9947372b2482c07b9f0fb8592198400a8b13
Dylan-Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2024-03-12 00:35:31 -0600

    Get modal working, needs to be restyled

--- src/app/components/authentication/signup.tsx
+++ src/app/components/authentication/signup.tsx
@@ -1,16 +1,40 @@
 import styles from '../../styles/signup.module.css';
+import { useState } from 'react';
 
-export default function Signup() {
+export default function Signup(
+    { setRegisterOpen, userDetails }: {
+        setRegisterOpen: Function,
+        userDetails: {
+            email: string,
+            password: string
+        }
+    }
+) {
+    const [inputs, setInputs] = useState({email: "", password: ""});
+
     return (
         <div className={styles.signupContainer}>
             <dialog className={styles.dialog}>
                 <form name="signupForm" action="dialog" className={styles.form}>
+                    <p className={styles.xButton} onClick={(e) => {
+                        e.preventDefault();
+
+                        setRegisterOpen(false);
+                    }}>X</p>
                     <div className={styles.inputRow}>
                         <label htmlFor="emailInput" className={styles.inputLabel}>
                             E-Mail:
                         </label>
-                        <input type="email" name="emailInput" className={styles.emailInput}
-                            placeholder="johndoe@gmail.com">
+                        <input type="email"
+                            name="emailInput"
+                            className={`${styles.emailInput} ${styles.formInput}`}
+                            placeholder="johndoe@gmail.com"
+                            content={inputs.email}
+                            onChange={(e) => {
+                                e.preventDefault();
+
+                                setInputs({...inputs, email: e.target.value});
+                            }}>
                         </input>
                     </div>
                     <div className={styles.inputRow}>
@@ -18,9 +42,35 @@ export default function Signup() {
                                Password:
                         </label>
                         <input type="password" name="passwordInput"
-                               className={styles.passwordInput}
-                               placeholder="Pass_word-1234">
+                            className={`${styles.passwordInput} ${styles.formInput}`}
+                            placeholder="Pass_word-1234"
+                            content={inputs.password}
+                            onChange={(e) => {
+                                e.preventDefault();
+
+                                setInputs({...inputs, password: e.target.value});
+                            }}>
                         </input>
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <button type="button" className={styles.formButton}
+                         onClick={(e) => {
+                                e.preventDefault();
+
+                                setRegisterOpen(false);
+                        }}>
+                            Cancel
+                        </button>
+                        <button type="submit" className={styles.formButton}
+                         onClick={(e) => {
+                            e.preventDefault();
+
+                            userDetails = inputs;
+
+                            setRegisterOpen(false);
+                        }}>
+                            Register!
+                        </button>
                     </div>
                 </form>
             </dialog>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,12 @@ import  { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import Signup from './components/authentication/signup';
 
 const firebaseConfig = {
+    apiKey: "AIzaSyBDn6KtWHcjq04dMHyvBhr6kg492EBF4wk",
+    authDomain: "booktrack-2d95a.firebaseapp.com",
+    projectId: "booktrack-2d95a",
+    storageBucket: "booktrack-2d95a.appspot.com",
+    messagingSenderId: "117581302631",
+    appId: "1:117581302631:web:2a7d8523e12b0e186b7dc2"
 };
 
 function openSetup(e: Event) {
@@ -53,7 +59,13 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export default function Home() {
-    const [registerOpen, setRegisterOpen] = useState(false);
+    const [registerDetails, setRegisterDetails] = useState({
+        open: false,
+        details: {
+            email: "",
+            password: "",
+        },
+    });
 
     const guidesRef = useRef() as MutableRefObject<HTMLDivElement>;
     function scrollToGuides(e: Event) {
@@ -68,10 +80,10 @@ export default function Home() {
     function registerOnClick(e: Event) {
         e.preventDefault();
 
-        if(registerOpen) {
-            setRegisterOpen(false);
+        if(registerDetails.open) {
+            setRegisterDetails({...registerDetails, open: false});
         } else {
-            setRegisterOpen(true);
+            setRegisterDetails({...registerDetails, open: true});
         }
     }
 
@@ -121,99 +133,95 @@ export default function Home() {
 
     return (
         <main className={styles.main}>
-                <div className={registerOpen ?
-                                `${styles.welcomeContainer} ${styles.backmost}`
-                                : styles.welcomeContainer}>
-                    <h1>Welcome to BookTrack!</h1>
-                    <p className={garamond500.className}>BookTrack is a
-                        user-friendly application designed to help you
-                        easily keep track of your reading. Regardless of
-                        what type of reader you are, this app makes it
-                        simple to organize your library, set personal goals,
-                        and stay motivated!
-                    </p>
+            {registerDetails.open && <Signup
+                setRegisterOpen={setRegisterDetails}
+                userDetails={registerDetails.details}
+            />}
+            <div className={styles.welcomeContainer}>
+                <h1>Welcome to BookTrack!</h1>
+                <p className={garamond500.className}>BookTrack is a
+                    user-friendly application designed to help you
+                    easily keep track of your reading. Regardless of
+                    what type of reader you are, this app makes it
+                    simple to organize your library, set personal goals,
+                    and stay motivated!
+                </p>
+            </div>
+            <div className={styles.typContainer}>
+                <h1>Track Your Progress</h1>
+                <h4>Manage your personal library efficiently and easily</h4>
+                <span>
+                    <button type="button" id="getStartedButton">Get Started! &rarr;</button>
+                </span>
+            </div>
+            <div ref={guidesRef} className={styles.guideContainer}>
+                <h1>Guides</h1>
+                <div className={styles.guidesContainer}>
+                    <button type="button" id="setupButton" className={styles.guideButton}>
+                        Setup
+                    </button>
+                    <button type="button" id="upgradeButton" className={styles.guideButton}>
+                        Upgrade
+                    </button>
+                    <button type="button" id="manageBookButton" className={styles.guideButton}>
+                        Manage a Book
+                    </button>
+                    <button type="button" id="manageListButton" className={styles.guideButton}>
+                        Manage a List
+                    </button>
+                    <button type="button" id="manageDBButton" className={styles.guideButton}>
+                        Manage Your Database
+                    </button>
+                    <button type="button" id="downloadDBButton" className={styles.guideButton}>
+                        Download Your Database
+                    </button>
                 </div>
-                <div className={styles.typContainer}>
-                    <h1>Track Your Progress</h1>
-                    <h4>Manage your personal library efficiently and easily</h4>
-                    <span>
-                        <button type="button" id="getStartedButton">Get Started! &rarr;</button>
-                    </span>
-                </div>
-                <div ref={guidesRef} className={styles.guideContainer}>
-                    <h1>Guides</h1>
-                    <div className={styles.guidesContainer}>
-                        <button type="button" id="setupButton" className={styles.guideButton}>
-                            Setup
-                        </button>
-                        <button type="button" id="upgradeButton" className={styles.guideButton}>
-                            Upgrade
-                        </button>
-                        <button type="button" id="manageBookButton" className={styles.guideButton}>
-                            Manage a Book
-                        </button>
-                        <button type="button" id="manageListButton" className={styles.guideButton}>
-                            Manage a List
-                        </button>
-                        <button type="button" id="manageDBButton" className={styles.guideButton}>
-                            Manage Your Database
-                        </button>
-                        <button type="button" id="downloadDBButton" className={styles.guideButton}>
-                            Download Your Database
-                        </button>
-                    </div>
-                </div>
-                <div className={styles.featuresContainer}>
-                    <h1>Features</h1>
-                    <h4>Discover how BookTrack can benefit your library.</h4>
-                    <div className={styles.featureButtons}>
-                        <button type="button" className={styles.featureButton}>
-                            <h3>Book Search</h3>
-                            <p className={garamond500.className}>
-                                Easily find a book you've already got in
-                                your list or find books to add to your list!
-                            </p>
-                        </button>
-                        <button type="button" className={styles.featureButton}>
-                            <h3>Reading Progress</h3>
-                            <p className={garamond500.className}>
-                                Track your current progress for each book
-                                with both percentage completion and page counts!
-                            </p>
-                        </button>
-                        <button type="button" className={styles.featureButton}>
-                            <h3>Custom Lists</h3>
-                            <p className={garamond500.className}>
-                                Create your own lists of books on top of
-                                the default lists we provide for you
-                            </p>
-                        </button>
-                        <button type="button" className={styles.featureButton}>
-                            <h3>Set and Track Goals</h3>
-                            <p className={garamond500.className}>
-                                Set personal goals and choose to have
-                                reminders sent to you regularly.
-                            </p>
-                        </button>
-                    </div>
-                    <button type="button" className={styles.dbButton}>
-                        <h3>Database Management</h3>
+            </div>
+            <div className={styles.featuresContainer}>
+                <h1>Features</h1>
+                <h4>Discover how BookTrack can benefit your library.</h4>
+                <div className={styles.featureButtons}>
+                    <button type="button" className={styles.featureButton}>
+                        <h3>Book Search</h3>
                         <p className={garamond500.className}>
-                            Your own personal database that you can manage
-                            and have complete control over!
+                            Easily find a book you've already got in
+                            your list or find books to add to your list!
                         </p>
+                    </button>
+                    <button type="button" className={styles.featureButton}>
+                        <h3>Reading Progress</h3>
                         <p className={garamond500.className}>
-                            Learn more about Turso
-                            <a className={garamond500.className}> here!</a>
+                            Track your current progress for each book
+                            with both percentage completion and page counts!
+                        </p>
+                    </button>
+                    <button type="button" className={styles.featureButton}>
+                        <h3>Custom Lists</h3>
+                        <p className={garamond500.className}>
+                            Create your own lists of books on top of
+                            the default lists we provide for you
+                        </p>
+                    </button>
+                    <button type="button" className={styles.featureButton}>
+                        <h3>Set and Track Goals</h3>
+                        <p className={garamond500.className}>
+                            Set personal goals and choose to have
+                            reminders sent to you regularly.
                         </p>
                     </button>
                 </div>
-                <div className={registerOpen ? styles.regular : styles.backmost}>
-                    <div className={registerOpen ? styles.modelCover : ''}></div>
-                    <div className={registerOpen ? styles.registerDiv : ''}>
-                        <Signup />
-                    </div>
-                </div>
+                <button type="button" className={styles.dbButton}>
+                    <h3>Database Management</h3>
+                    <p className={garamond500.className}>
+                        Your own personal database that you can manage
+                        and have complete control over!
+                    </p>
+                    <p className={garamond500.className}>
+                        Learn more about Turso
+                        <a className={garamond500.className}> here!</a>
+                    </p>
+                </button>
+            </div>
         </main>
     );
 }
