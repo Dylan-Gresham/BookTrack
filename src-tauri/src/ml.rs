d0074885f5cb6e707665e604af927d1fadbdb93b
Dylan-Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2024-11-06 14:52:09 -0700

    Rust code written

---
+++ src-tauri/src/ml.rs
@@ -0,0 +1,43 @@
+use pyo3::{prelude::*, prepare_freethreaded_python};
+
+fn predict(
+    py_lib: &str,
+    title: String,
+    author: String,
+    num_pages: usize,
+    synopsis: String,
+    list: String,
+) {
+    let result = Python::with_gil(|py| -> Result<f64, String> {
+        let module = PyModule::from_code_bound(py, py_lib, "lib.py", "lib.py").unwrap();
+        let predict_func = module.getattr("predict_rs").unwrap();
+
+        match predict_func.call1((title, author, num_pages, synopsis, list)) {
+            Ok(result) => Ok(result
+                .extract::<f64>()
+                .expect("Unable to convert Python response to a 64-bit float.")),
+            Err(e) => Err(e.to_string()),
+        }
+    });
+
+    match result {
+        Ok(num) => {
+            println!(
+                "There is a {:.2}% chance of the sample being the positive class.",
+                num * 100.0
+            );
+            println!(
+                "There is a {:.2}% chance of the sample being the negative class.",
+                (1.0 - num) * 100.0
+            );
+        }
+        Err(err) => eprintln!("{err}"),
+    }
+}
+
+fn prep_environment() -> String {
+    let py_lib = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/lib.py"));
+    prepare_freethreaded_python();
+
+    String::from_str(py_lib)
+}
