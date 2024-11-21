ba7369cdd6cd722a1769a5aa0bb5d3b95d41c04f
Brenek Harrison <41010693+BrenekH@users.noreply.github.com>
2024-11-21 01:42:38 -0700

    Closes #45 (#50)

--- src/app/lib/atoms.ts
+++ src/app/lib/atoms.ts
@@ -10,9 +10,17 @@ export interface UserInfo {
   userBooks: BookList;
 }
 
+export interface LibrarySortState {
+  sortBy: "title" | "author" | "pageCount" | "default";
+  order: "ascending" | "descending";
+}
+
 // Define atoms for user account details
 export const userInfoAtom = atom<UserInfo | null>(null);
 
 export const registeredBookListsAtom = atom<string[]>([]);
+
+// Shared state between Header component and library page for how the library should be sorted
+export const librarySortAtom = atom<LibrarySortState>({sortBy: "default", order: "descending"});
 
 export const clickedBookAtom = atom<BookType | undefined>(undefined);
